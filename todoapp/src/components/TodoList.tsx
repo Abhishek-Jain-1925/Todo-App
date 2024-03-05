d0b71dc54f7bbd20479a9fcb25aaa722740daa96
Abhishek-Jain-1925 <jainabhishek1925@gmail.com>
2024-03-05 18:23:05 +0530

    [send]:Add new files of Assignment5

--- todoapp/src/components/TodoList.tsx
+++ todoapp/src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface ITodoElement {
@@ -13,6 +13,9 @@ interface Props {
 
 const TodoList = ({ todos }: Props) => {
   const [checked, setChecked] = useState(false);
+  const [search, setSearch] = useState("");
+  //const [filteredTodos, setFilteredTodos] = useState(todos); //Optional, when using useEffect only
+  const [sortDirection, setSortDirection] = useState("0");
   const navigate = useNavigate();
 
   const handleStatus = (item: ITodoElement) => {
@@ -24,33 +27,43 @@ const TodoList = ({ todos }: Props) => {
     navigate(`/task/${item.id}`);
   };
 
-  return (
-    <div className="blog-list">
+  // useEffect(()=>{
+  //   setFilteredTodos(todos.filter((todo)=>todo.task.includes(search)))
+  // },[search, filteredTodos])
+  //or
+  const filterTodo = useMemo(()=>todos.filter((todo)=> todo.task.includes(search)),
+  [search,todos])
 
-      {todos.map((item) => (
+  const sortedTodos = useMemo(()=>
+    filterTodo.sort((a,b)=>sortDirection==="0" ? (a.task > b.task ? 1 : -1) : (a.task > b.task ? -1 : 1))
+  ,[filterTodo, sortDirection])
+
+  return (
+    <div className="blog-list text-center">
+      <div style={{display:"flex"}} className="justify-content-center">
+        <input type="text" placeholder="search todos" value={search} className="m-2" onChange={(e)=>setSearch(e.target.value)}/>
+
+        <label htmlFor="Sort" className="m-2">Sort :</label>
+        <select onChange={(e)=> setSortDirection(e.target.value)} className="m-2" >
+          <option value={"0"}>A-Z</option>
+          <option value={"1"}>Z-A</option>
+        </select>
+      </div>
+
+      {sortedTodos.map((item) => (
         <center>
-          <div
-            className="card w-50"
-            key={item.id}
-            style={{
+          <div className="card w-50" key={item.id}
+             style={{
               border: "0.1px solid grey",
               margin: "10px",
               cursor: "pointer",
-            }}
-          >
-            <div
-              className="card-header"
+            }} >
+            <div className="card-header"
               style={{
                 textDecoration: item.isComplete ? "line-through" : "none",
-              }}
-            >
+              }} >
               <div style={{display:"flex"}}>
-                <input
-                  type="checkbox"
-                  name={item.task}
-                  checked={item.isComplete}
-                  onChange={() => handleStatus(item)}
-                />{" "} &nbsp;&nbsp;
+                <input type="checkbox" name={item.task} checked={item.isComplete} onChange={() => handleStatus(item)} />{" "} &nbsp;&nbsp;
                 <h3 onClick={() => handleCard(item)}>
                 {item.task}
                 </h3>
